00f64ed116fd795bbfea6849f278e719d486f805
Andres Castro <andrescn20@gmail.com>
2023-07-23 19:21:16 -0600

    Agregar enlace a GitHub

--- src/App.jsx
+++ src/App.jsx
@@ -84,42 +84,50 @@ function App() {
 
 
   return (
-    <>
-      <h1> Convertidor de Base </h1>
-      <form action="" className='border-2 p-4 rounded-2xl my-4'>
-        <div className='flex flex-col space-y-3 mb-8'>
-          <h3 className=''>Base Original</h3>
-          <div className='flex justify-between' onChange={updateOriginal}>
-            <input type="radio" id="binario" name="original" value="2" checked={original == 2} />
-            <label htmlFor="binario">2 (Binario)</label>
-            <input type="radio" id="octal" name="original" value="8" checked={original == 8} />
-            <label htmlFor="octal">8 (Octal)</label>
-            <input type="radio" id="decimal" name="original" value="10" checked={original == 10} />
-            <label htmlFor="decimal">10 (Decimal)</label>
-            <input type="radio" id="hexadecimal" name="original" value="16" checked={original == 16} />
-            <label htmlFor="hexadecimal">16 (Hexadecimal)</label>
+      <div className='flex flex-col'>
+        <h1> Convertidor de Base </h1>
+        <form action="" className='border-2 p-4 rounded-2xl my-4'>
+          <div className='flex flex-col space-y-3 mb-8'>
+            <h3 className=''>Base Original</h3>
+            <div className='flex justify-between' onChange={updateOriginal}>
+              <input type="radio" id="binario" name="original" value="2" checked={original == 2} />
+              <label htmlFor="binario">2 (Binario)</label>
+              <input type="radio" id="octal" name="original" value="8" checked={original == 8} />
+              <label htmlFor="octal">8 (Octal)</label>
+              <input type="radio" id="decimal" name="original" value="10" checked={original == 10} />
+              <label htmlFor="decimal">10 (Decimal)</label>
+              <input type="radio" id="hexadecimal" name="original" value="16" checked={original == 16} />
+              <label htmlFor="hexadecimal">16 (Hexadecimal)</label>
+            </div>
+            <input className='text-center rounded-xl py-1 max-w-xl text-2xl hover:bg-slate-600' onChange={updateNumero} type="text" placeholder={`${numero}`} />
           </div>
-          <input className='text-center rounded-xl py-1 max-w-xl text-2xl' onChange={updateNumero} type="text" placeholder={`${numero}`} />
-        </div>
-        <div className='flex flex-col space-y-3 mb-6'>
-          <h3>Base Nueva</h3>
-          <div className='flex justify-between' onChange={updateNueva}>
-            <input type="radio" id="binario" name="nueva" value="2" checked={nueva == 2} />
-            <label htmlFor="binario">2 (Binario)</label>
-            <input type="radio" id="octal" name="nueva" value="8" checked={nueva == 8} />
-            <label htmlFor="octal">8 (Octal)</label>
-            <input type="radio" id="decimal" name="nueva" value="10" checked={nueva == 10} />
-            <label htmlFor="decimal">10 (Decimal)</label>
-            <input type="radio" id="hexadecimal" name="nueva" value="16" checked={nueva == 16} />
-            <label htmlFor="hexadecimal">16 (Hexadecimal)</label>
+          <div className='flex flex-col space-y-3 mb-6'>
+            <h3>Base Nueva</h3>
+            <div className='flex justify-between' onChange={updateNueva}>
+              <input type="radio" id="binario" name="nueva" value="2" checked={nueva == 2} />
+              <label htmlFor="binario">2 (Binario)</label>
+              <input type="radio" id="octal" name="nueva" value="8" checked={nueva == 8} />
+              <label htmlFor="octal">8 (Octal)</label>
+              <input type="radio" id="decimal" name="nueva" value="10" checked={nueva == 10} />
+              <label htmlFor="decimal">10 (Decimal)</label>
+              <input type="radio" id="hexadecimal" name="nueva" value="16" checked={nueva == 16} />
+              <label htmlFor="hexadecimal">16 (Hexadecimal)</label>
+            </div>
+            <div className='flex space-x-6'>
+              <button type='submit' className='hover:scale-105 bg-white grow text-black font-bold rounded-xl max-w-fit self-center'>Convertir </button>
+              <p className='bg-white grow px-4 self-center rounded-2xl py-1 text-black text-2xl'>{resultado}</p>
+            </div>
           </div>
-          <div className='flex space-x-6'>
-            <button type='submit' className='bg-white grow text-black font-bold rounded-xl max-w-fit self-center'>Convertir </button>
-            <p className='bg-white grow px-4 self-center rounded-2xl py-1 text-black text-2xl'>{resultado}</p>
-          </div>
-        </div>
-      </form>
-    </>
+        </form>
+        <a className='flex items-center justify-center' href='https://github.com/andrescn20/convertidor-bases'>
+          <p className='mx-2'>
+            andrescn20
+          </p>
+          <i class="devicon-github-original"></i>
+
+        </a>
+      </div>
+
   )
 }
 export default App
